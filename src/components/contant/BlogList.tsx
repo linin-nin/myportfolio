2116a502c1fa124e5314aa9af71ce68d2101bffc
Ninn04 <lynin722@gmail.com>
2024-02-17 22:55:57 +0700

    contant: update scroll animate and mouse move interact

--- src/components/contant/BlogList.tsx
+++ src/components/contant/BlogList.tsx
@@ -5,12 +5,18 @@ import Link from 'next/link'
 import { data } from '@/app/_data'
 import { ArrowUpRight } from 'lucide-react'
 import Image from 'next/image'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Blog = () => {
 
   const component = useRef(null)
+  const revealRef = useRef(null)
+  const itemsRef = useRef<Array<HTMLLIElement | null>>(null)
   const [currectItem, setcurrectItem] = useState<null | number>(null)
-  const contentimage = data.map((item) => item.image.original)
+  const contentimage = [...data, ...data, ...data].map((item) => item.image.original)
 
   const onmouseEnter = (index:number) => {
     setcurrectItem(index)
@@ -20,17 +26,75 @@ const Blog = () => {
   }
 
   useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const mousePos = { x: e.clientX, y: e.clientY}
-    }
+    const ctx = gsap.context(() => {
+      itemsRef.current?.forEach((item) => {
+        gsap.fromTo(item, {
+          opacity: 0,
+          y: 20
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.3,
+          ease: "eastic.out(1,0.3)",
+          stagger: 0.2,
+          scrollTrigger: {
+            trigger: item,
+            start: "top bottom-=100px",
+            markers: true,
+            end: "bottom center",
+            toggleActions: "play none none none"
+          }
+        })
+      })
+    } )
   })
+
+  const lastMousePos = useRef({x:0, y:0})
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const mousePos = { x: e.clientX, y: e.clientY + window.scrollY}
+
+      // calculate speeech
+
+      const speech = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2))
+
+      let ctx = gsap.context(() => {
+        if(currectItem !== null) {
+          const maxY = window.scrollY + window.innerHeight - 350
+          const maxX = window.innerWidth - 250
+
+          gsap.to(revealRef.current, {
+            x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
+            y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
+            rotaion: speech * (mousePos.x > lastMousePos.current.x ? 1: -1),
+            ease: "back.out(2)",
+            duration: 1.3,
+            opacity: 1
+          })
+        }
+        lastMousePos.current = mousePos
+        return () => ctx.revert()
+      }, component)
+    }
+    window.addEventListener("mousemove", handleMouseMove)
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+    }
+  }, [currectItem])
 
   return (
     <div ref={component}>
       <ul onMouseLeave={onmouseLeave} className='grid border-b border-b-slate-100'>
         {
-          data.map((item, index) => (
+          [...data, ...data, ...data].map((item, index) => (
               <li 
+                ref={(item) => {
+                  if(itemsRef.current) {
+                    itemsRef.current[index] = item
+                  }
+                 } }
                 key={index}
                 className='list-item opacity-0f'
                 onMouseEnter={() => onmouseEnter(index)}
@@ -51,7 +115,7 @@ const Blog = () => {
         }
       </ul>
       {/* Hover animate */}
-      <div className='hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg  bg-over bg-center opacity-0f transition-[background] duration-300'
+      <div ref={revealRef} className='hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg  bg-over bg-center opacity-0f transition-[background] duration-300'
           // style={{
           //   backgroundImage: currectItem !== null ? `url${contentimage[currectItem]}` : ""
           // }}
