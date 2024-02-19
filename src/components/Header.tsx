9b1db85e5e35a702844b8579a1545fb5e21a9c69
Sreynin <snin934@gmail.com>
2024-02-19 10:03:31 +0700

    update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
+import Button from './biography/Button'
 
 const Header = () => {
   return (
     <div className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
-        <nav>
-            <ul>
+        <nav className='bg-slate-50 text-slate-950 font-bold flex justify-between items-center py-3 px-5 rounded-xl'>
+            <div className='text-2xl'>
+                <Link href="/" aria-label='Home page'>
+                    Sreynin
+                </Link>
+            </div>
+            <ul className='flex gap-6 items-center justify-center'>
                 <li>
-                    <Link href="/" aria-label='Home page'>
-                        Sreynin
-                    </Link>
+                    <Button label='Anout' linkField='/about' showIcon={false} className='border-none'/>
+                </li>
+                <li className=' text-slate-500'>/</li>
+                <li>
+                    <Button label='Projects' linkField='/project' showIcon={false} className='border-none'/>
                 </li>
                 <li>
-                    <Link href="/about" aria-label='Home page'>
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/blog" aria-label='Home page'>
-                        Blog
-                    </Link>
+                    <Button label='Contact' linkField='/' className=''/>
                 </li>
             </ul>
         </nav>
--- src/components/biography/Button.tsx
+++ src/components/biography/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
 import  { ArrowUpRight } from "lucide-react"
+import Link from 'next/link';
 
 
 type ButtonProps = {
@@ -15,10 +16,12 @@ type ButtonProps = {
 const Button = ({linkField, label, showIcon = true, className}:ButtonProps) => {
   return (
     <button className={clsx("group relative flex text-slate-800 w-full items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold transition-transform ease-out hover:scale-105", className)}>
-        <span className='absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0'></span>
-        <span className='relative flex items-center justify-center gap-2'>
-          {label} {showIcon && <ArrowUpRight className='inline-block'/>}
-        </span>
+        <Link href={linkField}>
+          <span className='absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0'></span>
+          <span className='relative flex items-center justify-center gap-2'>
+            {label} {showIcon && <ArrowUpRight className='inline-block'/>}
+          </span>
+        </Link>
     </button>
   )
 }
